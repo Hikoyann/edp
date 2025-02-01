f401baa7d34dd695986bef135f056db0dcf9c5e0
Hikoyann <i21i022dl@ous.jp>
2025-02-02 04:26:45 +0900

    使ってない機能削除

--- src/pages/equipment/index.jsx
+++ src/pages/equipment/index.jsx
@@ -1,9 +1,4 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { signInWithPopup } from "firebase/auth";
-import { auth, database, provider } from "../../lib/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { ref, get, set, push } from "firebase/database";
-// import { QRCode } from 'qrcode.react';
 import QRCodeLib from "qrcode";
 import { Header } from "@/components/Header";
 import { Register } from "@/components/Register";
